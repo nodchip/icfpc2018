8548a7ad4156fe0e158f3c5e60a74a7ff7dfb1a2
Hisayori Noda <nodchip@gmail.com>
2018-07-21 18:29:48 +0900

    Changed to check with multiple problems.

--- continuous_integration.sh
+++ continuous_integration.sh
@@ -6,5 +6,8 @@ make -j || exit 1
 ./test test.exe --gtest_output=xml:test_result.xml || exit 1
 for engine in ${ENGINES}
 do
-    ./${engine} --model ../data/problems/LA001_tgt.mdl --engine default --info output.json --trace-output LA001_out.nbt || exit 1
+	for problem in ${PROBLEMS}
+	do
+		./${engine} --model ../data/problems/${problem}_tgt.mdl --engine default --info output.json --trace-output LA001_out.nbt || exit 1
+		done
 done
