48d85c6daad299c875d803361570ae389cdbb559
Hisayori Noda <nodchip@gmail.com>
2018-07-21 21:18:33 +0900

    Fixed command line.

--- continuous_integration.sh
+++ continuous_integration.sh
@@ -9,5 +9,5 @@ for engine in ${ENGINES}
 do
 	python3 model_to_trace.py --binary_file_path ../src/${engine} --input_model_directory_path ../data/problems --output_trace_file_directory_path ../tmp/trace --output_info_file_directory_path ../tmp/${engine} --jobs 72 || exit 1
 done
-python3 --default_info_directory_path ../result/default_info --info_directory_path_base ../tmp --engines "${ENGINES}" --input_model_directory_path ../data/problems --output_html_file_path compare_engine.html || exit 1
+python3 compare_engine.py --default_info_directory_path ../result/default_info --info_directory_path_base ../tmp --engines "${ENGINES}" --input_model_directory_path ../data/problems --output_html_file_path compare_engine.html || exit 1
 copy -f compare_engine.html /var/www/html/ || exit 1
