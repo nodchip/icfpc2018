34ff984ec1b3db6edbc2bcf3a45e376be1561c27
Hitoshi Yoshida <github@peria.jp>
2018-07-21 12:41:57 +0900

    Remove boost-parser

--- src/engine_main.cpp
+++ src/engine_main.cpp
@@ -1,32 +1,50 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <map>
+#include <string>
 // 3rd
-#include <boost/program_options.hpp>
 #include <nlohmann/json.hpp>
 // project
 #include "engine.h"
 #include "trace.h"
 #include "nmms.h"
 
+using Options = std::map<std::string, std::string>;
+
+// Simply parse command  line options. not accept "--foo=bar"
+Options ParseCommand(int argc, char** argv) {
+  Options options;
+
+  std::string key, value;
+  for (int i = 1; i < argc; ++i) {
+    std::string arg(argv[i]);
+    if (arg[0] != '-') {
+      // value
+      // ASSERT(key != "");
+      options[key] = arg;
+      key = "";
+      continue;
+    }
+
+    int c = arg.find_first_not_of("-");
+    key = arg.substr(c);
+    value = "";
+  }
+
+  return options;
+}
+
 int main(int argc, char** argv) {
-    namespace po = boost::program_options;
+    Options options = ParseCommand(argc, argv);
+    // ("help", "produce help message");
+    // ("model", po::value<std::string>(), "model.mdl");
+    // ("trace-output", po::value<std::string>(), "trace.nbt");
+    // ("engine", po::value<std::string>()->default_value("default"), "engine");
+    // ("info", po::value<std::string>()->default_value("info"), "info path");
 
-    po::options_description desc("Engine");
-    desc.add_options()
-        ("help", "produce help message")
-        ("model", po::value<std::string>(), "model.mdl")
-        ("trace-output", po::value<std::string>(), "trace.nbt")
-        ("engine", po::value<std::string>()->default_value("default"), "engine")
-        ("info", po::value<std::string>()->default_value("info"), "info path")
-    ;
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-        std::cout << desc << "\n";
+    if (options.count("help")) {
+        std::cout << "produce help message" << "\n";
         return 1;
     }
 
@@ -37,16 +55,17 @@ int main(int argc, char** argv) {
 
     EngineFunc engine;
     std::string engine_name;
-    if (vm["engine"].as<std::string>() == "default") {
+    if (options.count("engine") == 0 || options["engine"] == "default") {
         auto it = RegisterEngine::Engines().begin();
         engine = it->second;
         engine_name = it->first;
     } else {
-        auto it = RegisterEngine::Engines().find(vm["engine"].as<std::string>());
+        auto it = RegisterEngine::Engines().find(options["engine"]);
         if (it != RegisterEngine::Engines().end()) {
             it = RegisterEngine::Engines().begin();
         } else {
-            std::cout << "Failed to find engine: " << vm["engine"].as<std::string>() << std::endl;
+            std::cout << "Failed to find engine: "
+                      << options["engine"] << std::endl;
             return 2;
         }
         engine = it->second;
@@ -54,11 +73,11 @@ int main(int argc, char** argv) {
     }
 
     // load
-    if (!vm.count("model")) {
+    if (!options.count("model")) {
         std::cout << "--model required." << std::endl;
         return 3;
     }
-    auto model_path = vm["model"].as<std::string>();
+    auto model_path = options["model"];
     Matrix m = load_model(model_path);
     if (!m) {
         std::cout << "Failed to open model file: " << model_path << std::endl;
@@ -90,24 +109,24 @@ int main(int argc, char** argv) {
         exit_code = 4;
     }
 
-    if (vm.count("trace-output")) {
+    if (options.count("trace-output")) {
         // dump the result.
-        auto dump_model_path = vm["trace-output"].as<std::string>() + ".mdl";
-        auto dump_trace_path = vm["trace-output"].as<std::string>();
+        auto dump_model_path = options["trace-output"] + ".mdl";
+        auto dump_trace_path = options["trace-output"];
         dump_model(dump_model_path, sys.matrix);
 
         // trace.
         trace.output_trace(dump_trace_path);
     }
 
-    if (vm.count("info")) {
+    if (options.count("info")) {
         nlohmann::json j = {
             {"energy", sys.energy},
             {"consumed_commands", sys.consumed_commands},
             {"successful", is_successful},
             {"engine_name", engine_name},
         };
-        std::ofstream ofs(vm["info"].as<std::string>());
+        std::ofstream ofs(options["info"]);
         ofs << std::setw(4) << j;
     }
 
