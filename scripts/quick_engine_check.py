1b8e285ca6a540981040a1fc19dc5298de5cf9d9
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2018-07-21 15:07:24 -0700

    support A/D/R in quick_engine_check.py

--- scripts/quick_engine_check.py
+++ scripts/quick_engine_check.py
@@ -8,9 +8,11 @@ import multiprocessing
 import tqdm
 
 REPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))
-default_model_dir = os.path.join(REPO_DIR, 'data', 'problems')
+#default_model_dir = os.path.join(REPO_DIR, 'data', 'problems') # lightling
+default_model_dir = os.path.join(REPO_DIR, 'data', 'problemsF') # full
 default_trace_output_dir = 'tmp-quick-traces'
 default_info_output_dir = 'tmp-quick-info'
+default_energy_output_dir = 'tmp-quick-energy'
 
 def resolve_engine(engine_name):
     if os.path.isfile(engine_name):
@@ -31,10 +33,12 @@ def job(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('engine', help='engine executable')
-    parser.add_argument('--jobs', type=int, default=8, help='number of processes')
+    parser.add_argument('--jobs', type=int, default=2, help='number of processes')
     parser.add_argument('--model-dir', default=default_model_dir, help='directory containing models')
-    parser.add_argument('--trace-output-dir', default=default_trace_output_dir, help='directory containing models')
-    parser.add_argument('--info-output-dir', default=default_info_output_dir, help='directory containing models')
+    parser.add_argument('--trace-output-dir', default=default_trace_output_dir, help='directory to store trace')
+    parser.add_argument('--info-output-dir', default=default_info_output_dir, help='directory to store info')
+    parser.add_argument('--energy-output-dir', default=default_energy_output_dir, help='directory to store energy')
+    parser.add_argument('--type', default='ADR', help='A, D, R or combination of them')
 
     args = parser.parse_args()
 
@@ -50,23 +54,46 @@ if __name__ == '__main__':
     model_files.sort()
     commands = []
     for f in model_files:
-        name = re.sub(r'(LA\d+)_tgt\.mdl', r'\1', f)
+        mo = re.match(r'(?P<base>[LF](?P<type>A|D|R)\d+)_(src|tgt)\.mdl', f)
+        if mo is not None:
+            base = mo.groupdict()['base']
+            problem_type = mo.groupdict()['type']
+            if problem_type not in args.type:
+                continue
 
-        model_path = os.path.abspath(os.path.join(args.model_dir, f))
-        trace_path = os.path.abspath(os.path.join(args.trace_output_dir, '{}.nbt'.format(name)))
-        info_path = os.path.abspath(os.path.join(args.info_output_dir, '{}.json'.format(name)))
+            src_model_path = os.path.abspath(os.path.join(args.model_dir, '{}_src.mdl'.format(base)))
+            tgt_model_path = os.path.abspath(os.path.join(args.model_dir, '{}_tgt.mdl'.format(base)))
+            trace_path = os.path.abspath(os.path.join(args.trace_output_dir, '{}.nbt'.format(base)))
+            info_path = os.path.abspath(os.path.join(args.info_output_dir, '{}.json'.format(base)))
+            energy_path = os.path.abspath(os.path.join(args.energy_output_dir, '{}.energy.json'.format(base)))
 
-        cmds = [os.path.abspath(engine_path), '--model', model_path, '--trace-output', trace_path, '--info', info_path]
-        commands.append((cmds, engine_path, f))
+            if problem_type == 'A':
+                assert os.path.isfile(tgt_model_path)
+                cmds = [os.path.abspath(engine_path), '--model', tgt_model_path]
+            if problem_type == 'D':
+                assert os.path.isfile(src_model_path)
+                cmds = [os.path.abspath(engine_path), '--src-model', src_model_path]
+            if problem_type == 'R':
+                assert os.path.isfile(src_model_path)
+                assert os.path.isfile(tgt_model_path)
+                cmds = [os.path.abspath(engine_path), '--model', tgt_model_path, '--src-model', src_model_path]
 
-    with multiprocessing.Pool(args.jobs) as pool:
-        for (cmds, engine_path, f), res in tqdm.tqdm(pool.imap(job, commands), total=len(commands)):
-            if res.returncode != 0:
-                tqdm.tqdm.write('Fail: {}'.format(f))
-                error_results.append((f, res))
-                n_failed += 1
-            else:
-                n_succeeded += 1
+            cmds += ['--trace-output', trace_path, '--info', info_path, '--energy', energy_path]
+            commands.append((cmds, engine_path, f))
+
+    if args.jobs > 1:
+        pool = multiprocessing.Pool(args.jobs)
+        mapping = pool.imap
+    else:
+        mapping = map
+
+    for (cmds, engine_path, f), res in tqdm.tqdm(mapping(job, commands), total=len(commands)):
+        if res.returncode != 0:
+            tqdm.tqdm.write('Fail: {}'.format(f))
+            error_results.append((f, res))
+            n_failed += 1
+        else:
+            n_succeeded += 1
 
     if n_failed > 0:
         print('FAILED MODELS:')
