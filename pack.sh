9eeaaa441be40c606e85fa2c34a02183de8379cb
Hisayori Noda <nodchip@gmail.com>
2018-07-22 00:39:38 +0900

    Changed to exit on error.  Change a binary to be executed.

--- pack.sh
+++ pack.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 export WORKSPACE=`pwd`
-for engine in stupid_engine stupid_engine_v2 stupid_bbox_engine
+for engine in stupid_engine stupid_engine_v2 parallel_stupid_engine
 do
 	export BINARY_FILE_NAME=${engine}
-	./execute_engine.sh
+	./execute_engine.sh || exit 1
 done
 cd result/trace
 FILENAME=sanma`date +%Y%m%d%H%M%S`.zip
