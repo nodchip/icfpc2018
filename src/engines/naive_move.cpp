a3d68c6daa7663b05b5f6e779882a022f396dd2d
Hisayori Noda <nodchip@gmail.com>
2018-07-23 22:32:29 +0900

    Revert "Optimized naive_move()."
    This reverts commit dfbdffe33b2e39f2fc4db0381c3a0e7c63b321f3.

--- src/engines/naive_move.cpp
+++ src/engines/naive_move.cpp
@@ -8,23 +8,21 @@ static const Vec3 unitY(0, 1, 0);
 static const Vec3 unitZ(0, 0, 1);
 
 void naive_move(const Vec3& destination, Vec3& position, Trace& trace) {
-    Trace temp_trace;
     const Vec3 dz = (destination.z > position.z) ? unitZ : -unitZ;
     while (position.z != destination.z) {
-        temp_trace.push_back(CommandSMove{ dz });
+        trace.push_back(CommandSMove{ dz });
         position += dz;
     }
 
     const Vec3 dx = (destination.x > position.x) ? unitX : -unitX;
     while (position.x != destination.x) {
-        temp_trace.push_back(CommandSMove{ dx });
+        trace.push_back(CommandSMove{ dx });
         position += dx;
     }
     const Vec3 dy = (destination.y > position.y) ? unitY : -unitY;
     while (position.y != destination.y) {
-        temp_trace.push_back(CommandSMove{ dy });
+        trace.push_back(CommandSMove{ dy });
         position += dy;
     }
-    temp_trace.reduction_smove();
-    trace.insert(trace.end(), temp_trace.begin(), temp_trace.end());
 }
+
