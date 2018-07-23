4cdd781bba909945d2bd1d04561d5e242b78796d
Hisayori Noda <nodchip@gmail.com>
2018-07-23 19:32:27 +0900

    Fixed a directory path.  Added progress messages.

--- execute_engine.sh
+++ execute_engine.sh
@@ -5,6 +5,8 @@ git submodule update
 cd ${WORKSPACE}/src || exit 1
 make -j ${NUMBER_OF_PROCESSORS} || exit 1
 cd ${WORKSPACE}/scripts || exit 1
+
+echo Executing ${engine}
 python3 model_to_trace.py \
         --binary_file_names ${BINARY_FILE_NAME} \
         --binary_directory_path ../src \
@@ -14,8 +16,11 @@ python3 model_to_trace.py \
         --output_energy_file_parent_directory_path ../tmp/energy \
 		--timeout_sec 120 \
         --jobs ${NUMBER_OF_PROCESSORS} || exit 1
-python3 update_result.py --temp_trace_directory_path ../tmp/trace/${BINARY_FILE_NAME} \
-        --temp_info_directory_path ../tmp/info${BINARY_FILE_NAME} \
+
+echo Updating trace/info with the reuslt of ${engine}
+python3 update_result.py \
+		--temp_trace_directory_path ../tmp/trace/${BINARY_FILE_NAME} \
+        --temp_info_directory_path ../tmp/info/${BINARY_FILE_NAME} \
         --result_trace_directory_path ../resultF/trace \
         --result_info_directory_path ../resultF/info || exit 1
 rm -Rf ../tmp/trace/${BINARY_FILE_NAME}
