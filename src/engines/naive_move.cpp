dfbdffe33b2e39f2fc4db0381c3a0e7c63b321f3
Hisayori Noda <nodchip@gmail.com>
2018-07-23 22:26:45 +0900

    Optimized naive_move().

--- src/engines/naive_move.cpp
+++ src/engines/naive_move.cpp
@@ -8,21 +8,23 @@ static const Vec3 unitY(0, 1, 0);
 static const Vec3 unitZ(0, 0, 1);
 
 void naive_move(const Vec3& destination, Vec3& position, Trace& trace) {
+    Trace temp_trace;
     const Vec3 dz = (destination.z > position.z) ? unitZ : -unitZ;
     while (position.z != destination.z) {
-        trace.push_back(CommandSMove{ dz });
+        temp_trace.push_back(CommandSMove{ dz });
         position += dz;
     }
 
     const Vec3 dx = (destination.x > position.x) ? unitX : -unitX;
     while (position.x != destination.x) {
-        trace.push_back(CommandSMove{ dx });
+        temp_trace.push_back(CommandSMove{ dx });
         position += dx;
     }
     const Vec3 dy = (destination.y > position.y) ? unitY : -unitY;
     while (position.y != destination.y) {
-        trace.push_back(CommandSMove{ dy });
+        temp_trace.push_back(CommandSMove{ dy });
         position += dy;
     }
+    temp_trace.reduction_smove();
+    trace.insert(trace.end(), temp_trace.begin(), temp_trace.end());
 }
-
