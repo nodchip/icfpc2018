7b2c92423bae642a3ac8d710a07dee05f76850d3
Hisayori Noda <nodchip@gmail.com>
2018-07-23 22:22:26 +0900

    Moved naive_move to naive_move.h/cpp.

---
+++ src/engines/naive_move.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct Vec3;
+class Trace;
+
+void naive_move(const Vec3& destination, Vec3& position, Trace& trace);
--- src/engines/parallel_optimized_v5.cpp
+++ src/engines/parallel_optimized_v5.cpp
@@ -8,31 +8,13 @@
 #include "system.h"
 #include "state.h"
 #include "log.h"
+#include "naive_move.h"
 
 namespace {
 
-const Vec3 unitX(1, 0, 0);
-const Vec3 unitY(0, 1, 0);
-const Vec3 unitZ(0, 0, 1);
-
-void naive_move(const Vec3& destination, Vec3& position, Trace& trace) {
-    const Vec3 dz = (destination.z > position.z) ? unitZ : -unitZ;
-    while (position.z != destination.z) {
-        trace.push_back(CommandSMove{dz});
-        position += dz;
-    }
-
-    const Vec3 dx = (destination.x > position.x) ? unitX : -unitX;
-    while (position.x != destination.x) {
-        trace.push_back(CommandSMove{dx});
-        position += dx;
-    }
-    const Vec3 dy = (destination.y > position.y) ? unitY : -unitY;
-    while (position.y != destination.y) {
-        trace.push_back(CommandSMove{dy});
-        position += dy;
-    }
-}
+static const Vec3 unitX(1, 0, 0);
+static const Vec3 unitY(0, 1, 0);
+static const Vec3 unitZ(0, 0, 1);
 
 Trace single_stupid_solver(const System& system, const Matrix& tgt_matrix,
                            const Vec3& lower_bound, const Vec3& upper_bound,
