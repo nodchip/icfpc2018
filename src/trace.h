3edede5f405a1c1d45412a89edc6aaf8ed8703d5
Jun Okabe <fofof200@gmail.com>
2018-07-23 19:03:07 +0900

    Addinf reduction CommandSMove for Trace.

--- src/trace.h
+++ src/trace.h
@@ -12,6 +12,13 @@ class Trace : public std::deque<Command> {
   bool input_trace(std::string input_path);
   void print_detailed();
   Trace transpose();
+  void reduction_smove();
+  void print() {
+    std::cout << "Trace: " << std::endl;
+    for (const auto c : *this) {
+      std::cout << c << std::endl;
+    }
+  }
 
   Vec3 offset() const;
 };
