a23dc0fe330dc23af29e0c45e21ad8390d83ebc2
Hisayori Noda <nodchip@gmail.com>
2018-07-23 16:36:56 +0900

    Improved the parallelism of CI.

--- execute_engine.sh
+++ execute_engine.sh
@@ -6,14 +6,16 @@ cd ${WORKSPACE}/src || exit 1
 make -j ${NUMBER_OF_PROCESSORS} || exit 1
 cd ${WORKSPACE}/scripts || exit 1
 python3 model_to_trace.py \
-        --binary_file_path ../src/${BINARY_FILE_NAME} \
+        --binary_file_names ../src \
+        --binary_directory_path ${BINARY_FILE_NAME} \
         --input_model_directory_path ../data/problemsF \
-        --output_trace_file_directory_path ../tmp/trace \
-        --output_info_file_directory_path ../tmp/info \
-        --output_energy_file_directory_path ../tmp/energy \
+        --output_trace_file_parent_directory_path ../tmp/trace \
+        --output_info_file_parent_directory_path ../tmp/info \
+        --output_energy_file_parent_directory_path ../tmp/energy \
 		--timeout_sec 120 \
         --jobs ${NUMBER_OF_PROCESSORS} || exit 1
-python3 update_result.py --temp_trace_directory_path ../tmp/trace \
-        --temp_info_directory_path ../tmp/info \
+python3 update_result.py --temp_trace_directory_path ../tmp/trace/${BINARY_FILE_NAME} \
+        --temp_info_directory_path ../tmp/info${BINARY_FILE_NAME} \
         --result_trace_directory_path ../resultF/trace \
         --result_info_directory_path ../resultF/info || exit 1
+rm -Rf ../tmp/trace/${BINARY_FILE_NAME}
